b79df4ee868fe90c0f97598a513bf3fe7ff136a1
You Song Hou <ysongweb3@gmail.com>
2024-09-26 22:23:19 -0400

    feat: install 'wouter'

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,10 +1,20 @@
-
+import { Link, Route, Switch } from "wouter";
 
 function App() {
 
   return (
     <>
       <h1>Project Prism Showcase</h1>
+      <Link href="/users/bob">Profile</Link>
+      <Switch>
+
+        <Route path="/users/:name">
+          {(params) => <>Hello, {params.name}!</>}
+        </Route>
+
+        {/* Default route in a switch */}
+        <Route>404: No such page!</Route>
+      </Switch>
     </>
   )
 }
