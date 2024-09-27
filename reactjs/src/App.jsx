9d6cdb3776212cf3922a9808b3ec682bb0265906
You Song Hou <ysongweb3@gmail.com>
2024-09-27 01:25:37 -0400

    feat: install React Three Fiber

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,15 +1,24 @@
 import { Link, Route, Switch } from "wouter";
 
+import Example from "./page/Example";
+
 function App() {
 
   return (
     <>
       <h1>Project Prism Showcase</h1>
+
       <Link href="/users/bob">Profile</Link>
+      <Link href="/example">Example</Link>
+
       <Switch>
 
         <Route path="/users/:name">
           {(params) => <>Hello, {params.name}!</>}
+        </Route>
+
+        <Route path="/example">
+          <Example />
         </Route>
 
         {/* Default route in a switch */}
---
+++ reactjs/src/page/Example.jsx
@@ -0,0 +1,14 @@
+import { Canvas } from "@react-three/fiber";
+
+export default function Example() {
+  return (
+      <Canvas>
+        <mesh>
+          <boxGeometry args={[2, 2, 2]} />
+          <meshPhongMaterial />
+        </mesh>
+        <ambientLight intensity={0.1} />
+        <directionalLight position={[0, 0, 5]} color="red" />
+      </Canvas>
+  );
+}
