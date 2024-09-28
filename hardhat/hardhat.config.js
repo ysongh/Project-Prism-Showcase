5c40f71792116a81198bd5c55ba825b4b8911283
You Song Hou <ysongweb3@gmail.com>
2024-09-28 19:25:03 -0400

    feat: add the Scroll Sepolia Testnet public RPC

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -4,6 +4,13 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
+  networks: {
+    scrollSepolia: {
+      url: "https://sepolia-rpc.scroll.io/" || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
   paths: {
     artifacts: '../reactjs/src/artifacts',
     cache: '../reactjs/src/cache',
