086a2f8d9a914c1990f0c0a1849dee826d8a2544
You Song Hou <ysongweb3@gmail.com>
2024-09-27 21:40:29 -0400

    feat: add an image gallery template

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,15 +1,18 @@
 import { Link, Route, Switch } from "wouter";
 
 import Example from "./page/Example";
+import Gallery from "./page/Gallery";
 
 function App() {
 
   return (
     <>
       <h1>Project Prism Showcase</h1>
+      {/* <Gallery /> */}
 
       <Link href="/users/bob">Profile</Link>
       <Link href="/example">Example</Link>
+      <Link href="/gallery">Gallery</Link>
 
       <Switch>
 
@@ -21,8 +24,17 @@ function App() {
           <Example />
         </Route>
 
+
+        <Route path="/gallery">
+          <Gallery />
+        </Route>
+
+        <Route path="/gallery/item/:id">
+          <Gallery />
+        </Route>
+
         {/* Default route in a switch */}
-        <Route>404: No such page!</Route>
+        {/* <Route>404: No such page!</Route> */}
       </Switch>
     </>
   )
