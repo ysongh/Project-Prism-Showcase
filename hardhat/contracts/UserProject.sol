82f3b5bdb99a74cd1afa4c2cd5280320f073e575
You Song Hou <ysongweb3@gmail.com>
2024-09-28 18:39:34 -0400

    feat: add a name and description for the project to the contract

--- hardhat/contracts/UserProject.sol
+++ hardhat/contracts/UserProject.sol
@@ -7,13 +7,20 @@ contract UserProject {
   struct Project{
     string cid;
     string url;
+    string name;
+    string description;
     address from;
   }
 
   constructor() {}
 
-  function addProject(string memory _cid, string memory _url) external {
-    projects[msg.sender].push(Project(_cid, _url, msg.sender));
+  function addProject(
+    string memory _cid,
+    string memory _url,
+    string memory _name,
+    string memory _description
+  ) external {
+    projects[msg.sender].push(Project(_cid, _url, _name, _description, msg.sender));
   }
 
   function getProjects() external view returns (Project[] memory) {
