96043157e6146a4577b73284986dc4ac12b38f3f
You Song Hou <ysongweb3@gmail.com>
2024-09-30 03:35:29 -0400

    feat: add a link to the home page

--- reactjs/src/components/Navbar.jsx
+++ reactjs/src/components/Navbar.jsx
@@ -6,9 +6,9 @@ function Navbar() {
     <nav className="navbar">
       <div className="logo">Project Prism Showcase</div>
       <ul className="nav-links">
+        <Link href="/">Home</Link>
         <Link href="/gallery">Gallery</Link>
         <Link href="/create-project">Create Project</Link>
-        <li><a href="#contact">Contact</a></li>
       </ul>
     </nav>
   )
