15cc3169528a312910a3260c1bd84721e6c92f5a
You Song Hou <ysongweb3@gmail.com>
2024-10-06 22:33:58 -0400

    refactor: change the position of the images

--- reactjs/src/page/Gallery.jsx
+++ reactjs/src/page/Gallery.jsx
@@ -15,15 +15,15 @@ const images = [
   // Front
   { position: [0, 0, 1.5], rotation: [0, 0, 0], url: pexel(1103970) },
   // Back
-  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(416430) },
-  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(310452) },
+  { position: [-1, 0, -0.6], rotation: [0, 0, 0], url: pexel(416430) },
+  { position: [1, 0, -0.6], rotation: [0, 0, 0], url: pexel(310452) },
   // Left
-  { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: pexel(327482) },
-  { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: pexel(325185) },
-  { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: pexel(358574) },
+  { position: [-2, 0, 1.2], rotation: [0, Math.PI / 2.5, 0], url: pexel(327482) },
+  { position: [-2.15, 0, 3], rotation: [0, Math.PI / 2.5, 0], url: pexel(325185) },
+  // { position: [-3, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: pexel(358574) },
   // Right
-  { position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: pexel(227675) },
-  { position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: pexel(911738) },
+  { position: [2.4, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: pexel(227675) },
+  { position: [4, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: pexel(911738) },
   { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: pexel(1738986) }
 ];
 
