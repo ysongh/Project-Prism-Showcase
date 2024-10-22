1dab5d16d1bcee0b1b5cc6f89e21c8269fbe2b1e
You Song Hou <ysongweb3@gmail.com>
2024-10-22 01:37:37 -0400

    refactor: update useContracts to use AppKit

--- reactjs/src/utils/useContracts.js
+++ reactjs/src/utils/useContracts.js
@@ -1,11 +1,14 @@
+import { useAppKitProvider } from "@reown/appkit/react";
 import { ethers } from 'ethers';
 import UserProjectABI from "../artifacts/contracts/UserProject.sol/UserProject.json";
 
 const USERPROJECT_ADDRESS = "0x2Fe28e60d6272e833CeC32F6b1cdC9D4eE8744bd";
 
 export const useContracts = () => {
+  const { walletProvider } = useAppKitProvider('eip155');
+
   const initializeEthers = async () => {
-    const ethersProvider = new ethers.providers.Web3Provider(window.ethereum);
+    const ethersProvider = new ethers.providers.Web3Provider(walletProvider);
     const signer = ethersProvider.getSigner();
     return { ethersProvider, signer };
   };
